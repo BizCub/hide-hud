060b3cf163760c17e96c762c63dd08cb9e6a5bfd
BizCub <bizcubmine@gmail.com>
2025-10-12 09:00:17 +0200

    Update loader

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,7 +22,7 @@ stonecutter {
             for (version in versions) version("$version-$loader", version)
         }
         mc("fabric", "1.16.5")
-        mc("forge", "1.16.5", "1.18.2", "1.20.6")
+        mc("forge", "1.20.6", "1.18.2", "1.16.5")
         mc("neoforge", "1.21")
     }
 }
