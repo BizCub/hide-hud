e3967db89ed7091fc6cc361205eff1c99c658c78
BizCub <bizcubmine@gmail.com>
2026-01-24 14:41:39 +0200

    The loader has been updated again

--- buildSrc/src/main/kotlin/multiloader-loader.gradle.kts
+++ buildSrc/src/main/kotlin/multiloader-loader.gradle.kts
@@ -12,19 +12,20 @@ if (isForge || isNeoForge) {
     project.extra["mc.snapshot"] = null
 }
 
-if (isForge) {
-    if (!isClothConfigAvailable) {
-        project.extra["mod.cloth_config"] = "17.0.144"
-    }
-    if (mod.mc == "1.16.5") project.extra["pub.end"] = "1.16.5"
-    if (mod.mc == "1.21.1") project.extra["pub.start"] = "1.20.6"
-}
-
 if (isNeoForge) {
     val neoVers = mod.mc.substring(2)
-    val neoLoader = mod.dep("neoforge_loader")
+    val neoLoader = dep("neoforge_loader")
     val neoForge = if (neoVers.contains(".")) "$neoVers.$neoLoader" else "$neoVers.0.$neoLoader"
-    project.setProperty("deps.neoforge_loader", neoForge)
+    project.extra["dep.neoforge_loader"] = neoForge
 
-    if (mod.mc == "1.21.1") project.extra["pub.start"] = "1.21"
+    when (mod.mc) {
+        "1.21.1" -> project.extra["pub.start"] = "1.21"
+    }
+}
+
+if (isForge) {
+    when (mod.mc) {
+        "1.16.5" -> project.extra["pub.end"] = "1.16.5"
+        "1.21.1" -> project.extra["pub.start"] = "1.20.6"
+    }
 }
