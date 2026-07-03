17701cccacd195cd8b2b23acfa7c78a62f38d990
BizCub <bizcubmine@gmail.com>
2026-07-03 20:20:39 +0200

    Updated Gradle dependencies Added issue templates Fix publish versions

--- buildSrc/src/main/kotlin/multiloader.gradle.kts
+++ buildSrc/src/main/kotlin/multiloader.gradle.kts
@@ -1,6 +1,13 @@
 plugins {
     id("me.modmuss50.mod-publish-plugin")
+    id("dev.kikugie.fletching-table")
     id("com.bizcub.multiloader")
 }
 
-multiloader.init()
+multiloader {
+    if (isFabric) {
+        addDependency(
+            dependency = "net.fabricmc:fabric-loader:${getDep("fabric")}"
+        )
+    }
+}
